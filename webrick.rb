b14b19da1e9e474cdd034d09bc0e3471a0e1750a
Alexander von Below <Alex@vonBelow.Com>
2010-04-28 22:06:12 +0200

    JF monkeypatched the webrick, now it is working

--- webrick.rb
+++ webrick.rb
@@ -20,6 +20,47 @@ end
 require 'webrick'
 include WEBrick
 
+module WEBrick
+  class HTTPRequest
+    def read_request_line(socket)
+      @request_line = read_line(socket, 1024) if socket
+      if @request_line.size >= 1024 and @request_line[-1, 1] != LF
+        raise HTTPStatus::RequestURITooLarge
+      end
+      @request_time = Time.now
+      raise HTTPStatus::EOFError unless @request_line
+      #if /^(\S+)\s+(\S+)(?:\s+HTTP\/(\d+\.\d+))?\r?\n/mo =~ @request_line
+	  if /^(\S+)\s+(\S+)(?:\s+HTTP\/(\d+\.\d+))/mo =~ @request_line
+        @request_method = $1
+        @unparsed_uri   = $2
+        @http_version   = HTTPVersion.new($3 ? $3 : "0.9")
+      else
+        rl = @request_line.sub(/\x0d?\x0a\z/o, '')
+        raise HTTPStatus::BadRequest, "XXX bad Request-Line `#{rl}'."
+      end
+    end
+
+	def _read_data(io, method, *arg)
+		puts "in read_data"
+      begin
+        WEBrick::Utils.timeout(@config[:RequestTimeout]){
+			puts "method: #{method}"
+			begin
+				return io.__send__(method, *arg)
+			rescue Errno::EAGAIN => e
+				puts e
+				return nil
+			end
+        }
+      rescue Errno::ECONNRESET
+        return nil
+      rescue TimeoutError
+        raise HTTPStatus::RequestTimeout
+      end
+    end
+  end
+end
+
 hello_proc = lambda do |req, resp|
   resp['Content-Type'] = "text/html"
   resp.body = %{
